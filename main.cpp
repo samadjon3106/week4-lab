3c71d4602dafc3ecdc7f398afc4e11aba01f43c7
s.sayfullayev <s.sayfullayev@newuu.uz>
2024-09-18 12:39:32 +0500

    git init

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    /*int x;
+    cin>>x;
+     if (x>=0) {
+         cout << "x is postive" << endl;
+     }else{
+         if (x<0){
+             cout<<"x is negative"<<endl;
+         }
+     }
+   */
+
+/*int x;
+  cin>>x;
+   if (x<0 && x%2==0) {
+ cout << "x is negative and even " << endl;
+  } else if (x >= 0) {
+           cout << "x is positive and odd" << endl;
+}
+ }
+
+    /*int a, b, c, max;
+    cout << "Enter three numbers" << endl;
+    cin >> a >> b >> c;
+    max = a;
+    if (a > b && a > c) {
+        cout << a << " is the largest" << endl;
+    } else {
+        if (b > a && b > c) {
+            cout << b << " is the largest" << endl;
+        } else {
+
+        }
+    }
+        return 0;*/
+    /* problem 4
+      int x, y;
+    cin >> x >> y;
+    if (x % y == 0){
+        cout << x << "is divisible by<< y <<" << endl;
+    }else{
+        if (x%y!=0) {
+            cout << x << " is not divisible by "<<y<<endl;
+        }
+
+}*/
+
+    /*
+     * problem 5
+     * int x;
+cout<< "a given year is a leap"<<endl;
+cin>> x;
+if ( x%4==0){
+    cout<< x<<" is a leap year"<< endl;
+}else{
+    if ( x%100!=0 && x%400!=0){
+    cout<<x<<" is not a leap year"<<endl;
+    }
+}*/
+    double a, b,c;
+    cout<<"Enter coefficients a, b, and c:";
+    cin>>a>>b>>c;
+    double discriminant= b*b-4*a*c;
+    if (discriminant>0){
+        double root1=(-b+sqrt(discriminant))/(2*a)
+    }
+    if
+}
