830c4e2a261e7e4d00d0a864e8688e17e2fceed9
samadjon3106 <s.sayfullayev@newuu.uz>
2024-11-13 12:57:00 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,131 +1,148 @@
 #include <iostream>
+#include <string>
 #include <cmath>
 using namespace std;
-int main() {
-    /*int x;
-    cin>>x;
-     if (x>=0) {
-         cout << "x is postive" << endl;
-     }else{
-         if (x<0){
-             cout<<"x is negative"<<endl;
-         }
-     }
-   */
 
-/*int x;
-  cin>>x;
-   if (x<0 && x%2==0) {
- cout << "x is negative and even " << endl;
-  } else if (x >= 0) {
-           cout << "x is positive and odd" << endl;
+//const int SIZE = 4;
+//
+//
+//double sumColumn(const double m[][SIZE], int rowSize, int columnIndex) {
+//    double sum = 0.0;
+//    for (int i = 0; i < rowSize; i++) {
+//        sum += m[i][columnIndex];
+//    }
+//    return sum;
+//}
+//
+//int main() {
+//    double m[5][SIZE] = {
+//            {5, 3, 1, 6},
+//            {-5, 2, 2, 5},
+//            {6, 6, 1, 6},
+//            {-3, 11, 2, 55},
+//            {0, 4, 0, 0}
+//    };
+//
+//    int rowSize = 5;
+//
+//    for (int col = 0; col < SIZE; col++) {
+//        cout << "sum of column " << col << "=" << sumColumn(m, rowSize, col) << endl;
+//    }
+//
+//    return 0;
+//}
+//
+
+
+
+
+
+//const int SIZE=4;
+//
+//double sumMajorDiaganal(const double m[][SIZE]){
+//  double   sum=0.0;
+//    for(int i=0; i<SIZE; i++ ){
+//        sum+=m[i][i];
+//    }
+//return sum;
+//}
+//int main(){
+//    double m[4][SIZE]{
+//            {1.0, 2.0, 3.0, 4.0},
+//            {5.0, 6.0, 7.0, 8.0},
+//            {9.0, 10.0, 11.0 , 12.0},
+//            {13.0,14.0,15.0, 16.0}
+//    };
+//        cout<< sumMajorDiaganal( m)<<endl;
+//
+//    return 0;
+//}
+
+//const int N = 4;
+//void multiplyMatrix(const double a[][N], const double b[][N], double c[][N]) {
+//    for (int i = 0; i < N; i++) {
+//        for (int j = 0; j < N; j++) {
+//            c[i][i] = 0;
+//            for (int k = 0; k < N; k++) {
+//                c[i][j]+= a[i][k] * b[j][k];
+//            }
+//        }
+//    }
+//}
+//int main() {
+//    double M1[4][N] = {
+//            {1.0,  2.0,  3.0,  4.0},
+//            {5.0,  6.0,  7.0,  8.0},
+//            {9.0,  10.0, 11.0, 12.0},
+//            {13.0, 14.0, 15.0, 16.0}
+//    };
+//    double M2[4][N] = {
+//            {0.5,  1.5,  2.5,  3.5},
+//            {4.5,  5.5,  6.5,  7.5},
+//            {8.5,  9.5,  10.5, 11.5},
+//            {12.5, 13.5, 14.5, 15.5}
+//
+//    };
+//    double C[4][N];
+//    multiplyMatrix(M1, M2, C);
+//
+//    for (int i = 0; i < N; i++) {
+//        for (int j = 0; j < N; j++) {
+//            cout << C[i][j] << " ";
+//        }
+//        cout << endl;
+//    }
+//    return 0;
+//}
+
+
+
+
+//void search( string& s, char& key) {
+//        for (char ch: s) {
+//        if (ch == key) {
+//            cout << "Found" << endl;
+//            return;
+//        }
+//    }
+//    cout << "Not found" << endl;
+//}
+//  int main(){
+//  string s;
+//  char key;
+//
+//  cout<< "Enter the word: ";
+//  getline(cin, s);
+//
+//  cout<<"Enter the character your are looking for: ";
+//  cin>>key;
+//
+//  search(s, key);
+//
+//  return 0;
+//}
+
+
+int counts(const string& s, char a) {
+    int count=0;
+    for (char ch: s) {
+        if (ch == a) {
+            count++;
+        }
+    }
+    return count;
 }
- }
 
-    /*int a, b, c, max;
-    cout << "Enter three numbers" << endl;
-    cin >> a >> b >> c;
-    max = a;
-    if (a > b && a > c) {
-        cout << a << " is the largest" << endl;
-    } else {
-        if (b > a && b > c) {
-            cout << b << " is the largest" << endl;
-        } else {
+        int main(){
+    string s;
+    char a;
+    cout<<" ENTER the word:";
+    getline(cin, s);
 
-        }
-    }
-        return 0;*/
-    /* problem 4
-      int x, y;
-    cin >> x >> y;
-    if (x % y == 0){
-        cout << x << "is divisible by<< y <<" << endl;
-    }else{
-        if (x%y!=0) {
-            cout << x << " is not divisible by "<<y<<endl;
-        }
+    cout<<"enter the character: ";
+    cin>>a;
 
-}*/
-
-    /*
-     * problem 5
-     * int x;
-cout<< "a given year is a leap"<<endl;
-cin>> x;
-if ( x%4==0){
-    cout<< x<<" is a leap year"<< endl;
-}else{
-    if ( x%100!=0 && x%400!=0){
-    cout<<x<<" is not a leap year"<<endl;
-    }
-}*/
-    /* double a, b, c;
-     cout << "Enter coefficients a, b, c: ";
-     cin >> a >> b >> c;
-
-     if (a == 0) {
-         cout << "Coefficient 'a' must not be zero." << endl;
-         return 1;
-     }
-
-     double discriminant = b * b - 4 * a * c;
-
-     cout << fixed << setprecision(6); // Set precision for output
-
-     if (discriminant > 0) {
-         // Two real roots
-         double x1 = (-b + sqrt(discriminant)) / (2 * a);
-         double x2 = (-b - sqrt(discriminant)) / (2 * a);
-         cout << "x1=" << x1 << " x2=" << x2 << endl;
-     } else if (discriminant == 0) {
-         // One real root
-         double x1 = -b / (2 * a);
-         cout << "x1=x2=" << x1 << endl;
-     } else {
-         // Complex roots
-         double realPart = -b / (2 * a);
-         double imaginaryPart = sqrt(-discriminant) / (2 * a);
-         cout << "x1=" << realPart << "+" << imaginaryPart << "i "
-              << "x2=" << realPart << "-" << imaginaryPart << "i" << endl;
-     }
-
-     return 0;
- } */
-     /*problem7
-    int a, b, c;
-    cout << "Enter coefficients a, b, c:" << endl;
-    cin >> a >> b >> c;
-    if (a + b >= c || a + c >= b || b + c >= a) {
-        cout << " The triangle is valid";
-    } else { cout << " The triangle is not valid"<<endl; }*/
-     /*
-     char ch;
-        // Input a character
-        cout << "Enter a character: ";
-        cin >> ch;
-
-        // Check if the character is an uppercase letter
-        if (ch >= 'A' && ch <= 'Z') {
-            cout << "Uppercase alphabet" << endl;
-        }
-            // Check if the character is a lowercase letter
-        else if (ch >= 'a' && ch <= 'z') {
-            cout << "Lowercase alphabet" << endl;
-        }
-            // If it's neither, it's not an alphabet
-        else {
-            cout << "It is not an alphabet" << endl;
-        }  return 0;
-        */
-     int x, y;
-     cin>> x>>y;
-    double distance = sqrt(pow(x, 2) + pow(y, 2));
-     if ( x<5 && y<5) {
-         cout << " Point (" <<x<<","<< y<<") is in circle";
-     }else if ( x>5 && y>5) {
-         cout << " Point ("<<x<<","<< y<<") is not in circle"<< endl;
-     }
-    return 0;
-
+   int result= counts(s,a);
+   cout<< result<<endl;
+return 0;
 }
